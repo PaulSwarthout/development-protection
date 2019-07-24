383208c38c61f0777b3e545479f946dbaed8e19f
{Paul Swarthout} <{paul@paulswarthout.com}>
2019-07-24 12:32:10 -0500

    This works great!! All my symlinked folders (in Windows with MKLINK /J SYMLINK PATH_TO_FOLDER) that have a .git subfolder do not show the 'Delete' link for a plugin that has been deactivated.

--- development-protection.php
+++ development-protection.php
@@ -1,15 +1,24 @@
 <?php
 /**
  * Plugin Name:     Development Protection
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:
+ * Description:     Prevent WordPress from deleting your symlinked development environments for your plugins. Hides the 'Delete' link on the plugins page for any plugin folder that has a .git subfolder. THIS MUST STAY ACTIVATED AND INSTALLED TO PROTECT YOUR SYMLINKed DEVELOPMENT FOLDERS.
+ * Author:          Paul Swarthout
+ * Author URI:      http://www.paulswarthout.com
  * Text Domain:     development-protection
- * Domain Path:     /languages
- * Version:         0.1.0
- *
- * @package         Development_Protection
+ * Version:         0.3.0
  */
 
-// Your code starts here.
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+add_filter( 'plugin_action_links', 'look_see', 10, 2 );
+
+function look_see( $actions, $plugin_file ) {
+	if (array_key_exists('delete', $actions) ) {
+		$full_path = dirname(dirname(__FILE__) . "/../" . $plugin_file) . "/.git";
+		if (file_exists($full_path)) {
+			unset($actions['delete']);
+		}
+	}
+	return $actions;
+}
--- readme.txt
+++ readme.txt
@@ -14,7 +14,8 @@ Plugin will block the WordPress plugins page from deleting plugins with a .git s
 
 Have you ever clicked the delete link on the WordPress plugins page for a plugin that you're developing and watched as WordPress
 nicely deleted your symlinked folder and all of your git history? If you're lucky enough to have a copy on GitHub or BitBucket,
-you could just pull it down again, but still, you've just lost any and all changes that you've made since your last push.
+you could just pull it down again, but still, you've just lost any and all changes that you've made since your last push, and the local git repository, and
+any and all versioning history.
 
 The Development-Protection plugin helps solve that problem. It will remove the ability to 'Delete' a plugin from the WordPress plugins page
 that has a .git sub-folder. It cannot detect that a particular folder is symlinked, but it can recognize that it's a development folder by the presence of the .git folder.
