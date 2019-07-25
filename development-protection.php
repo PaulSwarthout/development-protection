cbf2c498d1fe618538e7d52ac9b9f6becc5a3b98
{Paul Swarthout} <{paul@paulswarthout.com}>
2019-07-25 17:36:11 -0500

    Cleaned up the code to be closer to the coding standards.
    Renamed the 'look_see' function to be more meaningful: 'check_for_git_folder'.
    
    Changed the detection method for the .git folder.

--- development-protection.php
+++ development-protection.php
@@ -4,25 +4,22 @@
  * Plugin URI:
  * Description:     Prevent WordPress from deleting your symlinked development environments for your plugins. Hides the 'Delete' link on the plugins page for any plugin folder that has a .git subfolder. THIS MUST STAY ACTIVATED AND INSTALLED TO PROTECT YOUR SYMLINKed DEVELOPMENT FOLDERS.
  * Author:          Paul Swarthout
- * Author URI:      http://www.paulswarthout.com
+ * Author URI:      http://paulswarthout.com/wordpress/
  * Text Domain:     development-protection
- * Version:         0.3.0
+ * Version:         0.3.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-add_filter( 'plugin_action_links', 'look_see', 10, 2 );
+add_filter( 'plugin_action_links', 'check_for_git_folder', 10, 2 );
 
-function look_see( $actions, $plugin_file ) {
-	if (array_key_exists('delete', $actions) ) {
-		/*
-		 * Many WordPress functions and constants, such as wp_plugin_dir() and WP_PLUGIN_DIR
-		 * Do not yet exist when this function is called. We have to figure out the full path
-		 * manually.
-		 */
-		$full_path = dirname(dirname(__FILE__) . "/../" . $plugin_file) . "/.git";
-		if (file_exists($full_path)) {
-			unset($actions['delete']);
+function check_for_git_folder( $actions, $plugin_file ) {
+	if ( array_key_exists( 'delete', $actions ) ) {
+
+		$full_path = dirname( WP_PLUGIN_DIR . "/{$plugin_file}" ) . "/.git";
+
+		if ( file_exists( $full_path ) ) {
+			unset( $actions['delete'] );
 		}
 	}
 	return $actions;
